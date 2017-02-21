751d53967354c33d8f2ac88cc722afdb393a2c6e
Sven Depondt <sven@b-bright.be>
2017-02-22 00:30:48 +0100

    Refactor string to timestamp using Dates from Sugar.js
    Replace the npm package date-util with a more advanced and more up to
    date libaray, Sugar.js

--- src/string.js
+++ src/string.js
@@ -1,73 +1,48 @@
 // load package date-util
-require('date-util')
-
-// default in seconds
-const defaultTimestampUnit = 'seconds';
-
-// custom time string formats
-const translateCustomTime = {
-  "now": "+0 days",
-  "today": "+0 days",
-  "yesterday": "-1 day",
-  "tomorrow": "+1 day",
-  "day after tomorrow": "+2 days"
-}
-
+require('../libs/sugar-date')
 
 module.exports = (pluginContext) => {
-  return {
-    respondsTo: (query) => {
-      return query.match(/[a-zA-z\s]/)
-    },
-    search: (query, env = {}) => {
-      return new Promise((resolve, reject) => {
-        // set timestamp unit
-        var timestampUnit = defaultTimestampUnit;
+    return {
+        respondsTo: (query) => {
+            return query.match(/./)
+        },
+        search: (query = '', env = {}) => {
+            // trim & convert to lowercase query string
+            query = query.toLowerCase().trim();
 
-        // update timestamp unit to milliseconds
-        if (env['timestampUnit'] && env['timestampUnit'] == 'milliseconds') {
-          timestampUnit = 'milliseconds';
+            // default settings
+            let timestampUnit = 'seconds';
+
+            // override timestamp unit
+            if (env['timestampUnit'] && env['timestampUnit'] == 'milliseconds') {
+                timestampUnit = 'milliseconds';
+            }
+
+            // create Sugar Date
+            var sugarDate = Sugar.Date.create(query);
+
+            // check if valid date
+            if (!Sugar.Date.isValid(sugarDate)) {
+                return Promise.reject();
+            }
+
+            // set timestamp format
+            let timestampFormat = timestampUnit == 'milliseconds' ? '{x}' : '{X}';
+
+            // set timestamp
+            const ts = Sugar.Date.format(sugarDate, timestampFormat, {fromUTC: true});
+
+            // return results
+            return new Promise((resolve, reject) => {
+                resolve([
+                    {
+                        icon: 'fa-clock-o',
+                        title: ts,
+                        subtitle: `Select to copy the timestamp in ${timestampUnit}.`,
+                        value: ts,
+                    }
+                ])
+            })
         }
-
-        // convert search query to lowercase to match with custom time strings
-        query = query.toLowerCase().trim();
-
-        // try to translate custom time strings
-        query = translateCustomTime[query] || query;
-
-        // get divider based on timestampUnit
-        var divider = timestampUnit == 'seconds' ? 1000 : 1;
-
-        // create Date object
-        var date = new Date().strtotime(query);
-
-        // get timestamp from string
-        var timestamp = Math.floor(date.getTime() / divider);
-
-        // get timestamp from string without time
-        var timestampStartDay = Math.floor(date.setHours(0,0,0,0) / divider);
-
-        // set timestamp
-        const value = timestamp;
-
-        // set timestamp start of the day
-        const valueStart = timestampStartDay;
-
-        resolve([
-          {
-            icon: 'fa-clock-o',
-            title: value,
-            subtitle: 'Timestamp: select to copy to clipboard.',
-            value: value,
-          },
-          {
-            icon: 'fa-clock-o',
-            title: valueStart,
-            subtitle: 'Timestamp start of the day: select to copy to clipboard.',
-            value: valueStart,
-          }
-        ])
-      })
-    },
-  }
+    }
 }
